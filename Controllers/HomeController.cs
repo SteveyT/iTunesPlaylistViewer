660f42083836d27c7374f7ee6cb785c7390d37b2
Stephen Tierney <stevehtee@yahoo.co.uk>
2013-01-26 22:46:54 +0000

    Some cleanup

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,133 +21,13 @@ namespace iTunesPlaylistViewer.Controllers
             return View();
         }
 
-        //
-        // GET: /Home/Details/5
-
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        //
-        // GET: /Home/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Home/Create
-
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Home/Edit/5
-
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Home/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Home/Delete/5
-
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Home/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
         [HttpPost]
         public ActionResult Upload(HttpPostedFileBase file)
         {
             var output = new StringBuilder();
             if (ModelState.IsValid)
             {
-                var settings = new XmlReaderSettings();
-                settings.IgnoreComments = true;
-                settings.IgnoreWhitespace = true;
-                settings.IgnoreComments = true;
-                settings.ValidationType = ValidationType.None;
-                settings.DtdProcessing = DtdProcessing.Ignore;
-
-                using (var reader = XmlReader.Create(file.InputStream, settings))
-                {
-                    var ws = new XmlWriterSettings {Indent = true};
-
-                    using (var writer = XmlWriter.Create(output, ws))
-                    {
-                        while (reader.Read())
-                        {
-                            switch (reader.NodeType)
-                            {
-                                case XmlNodeType.Element:
-                                    writer.WriteStartElement(reader.Name);
-                                    break;
-                                case XmlNodeType.Text:
-                                    writer.WriteString(reader.Value);
-                                    break;
-                                case XmlNodeType.XmlDeclaration:
-                                case XmlNodeType.ProcessingInstruction:
-                                    writer.WriteProcessingInstruction(reader.Name, reader.Value);
-                                    break;
-                                case XmlNodeType.Comment:
-                                    writer.WriteComment(reader.Value);
-                                    break;
-                                case XmlNodeType.EndElement:
-                                    writer.WriteFullEndElement();
-                                    break;
-                            }
-                        }
-                    }
-                }
+                GetXmlDocument(file, output);
             }
 
             var xmldoc = new XmlDocument();
@@ -162,6 +42,45 @@ namespace iTunesPlaylistViewer.Controllers
             }
             
             return View(model);
+        }
+
+        private static void GetXmlDocument(HttpPostedFileBase file, StringBuilder output)
+        {
+            var settings = new XmlReaderSettings {IgnoreComments = true, IgnoreWhitespace = true};
+            settings.IgnoreComments = true;
+            settings.ValidationType = ValidationType.None;
+            settings.DtdProcessing = DtdProcessing.Ignore;
+
+            using (var reader = XmlReader.Create(file.InputStream, settings))
+            {
+                var ws = new XmlWriterSettings {Indent = true};
+
+                using (var writer = XmlWriter.Create(output, ws))
+                {
+                    while (reader.Read())
+                    {
+                        switch (reader.NodeType)
+                        {
+                            case XmlNodeType.Element:
+                                writer.WriteStartElement(reader.Name);
+                                break;
+                            case XmlNodeType.Text:
+                                writer.WriteString(reader.Value);
+                                break;
+                            case XmlNodeType.XmlDeclaration:
+                            case XmlNodeType.ProcessingInstruction:
+                                writer.WriteProcessingInstruction(reader.Name, reader.Value);
+                                break;
+                            case XmlNodeType.Comment:
+                                writer.WriteComment(reader.Value);
+                                break;
+                            case XmlNodeType.EndElement:
+                                writer.WriteFullEndElement();
+                                break;
+                        }
+                    }
+                }
+            }
         }
 
         private Track parseTrack(XmlNode node)
@@ -202,29 +121,5 @@ namespace iTunesPlaylistViewer.Controllers
 
             return track;
         }
-
-        public List<string> AllKeys()
-        {
-            var list = new List<string>
-                {
-                    "Track ID",
-                    "Name",
-                    "Artist",
-                    "Genre",
-                    "Kind",
-                    "Album",
-                    "Size",
-                    "Total Time",
-                    "Year",
-                    "Date Modified",
-                    "Date Added",
-                    "Bit Rate",
-                    "Sample Rate",
-                    "Play Count",
-                    "Rating",
-                    "Album Rating"
-                };
-            return list;
-        } 
     }
 }
